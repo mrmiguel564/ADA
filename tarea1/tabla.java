f794a8edddcae5ec1c9059dbf1c33d3ce7a0d464
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-10-10 14:03:31 -0300

    asda

--- tarea1/tabla.java
+++ tarea1/tabla.java
@@ -1,10 +1,9 @@
 package tarea1;
 public class tabla(){
-     		public contenedor posiciones[][];
+     		public contenedor posiciones[][] = new contenedor [][] ;
      		
- 			public contenedor(boolean variables[],int costo[]){
- 					// implementar un metodo que segun las variables entrantes y el costo calcule la combinacion mas barata
-
+ 			public tabla(){
+ 					// 
  			
  				}
      }
