850f5cb84ee5083cd61c354b1f4387164cd31834
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-10-10 13:45:28 -0300

    +new class -delete class contenedor from main

---
+++ contenedor.java
@@ -0,0 +1,11 @@
+package tarea1;
+public class contenedor(){
+     		public int valor;
+     		public int cantidad[];
+ 			public boolean variables[];
+ 			
+ 			public contenedor(boolean variables[],int costo[]){
+ 					
+
+ 				}
+     }
---
+++ tarea1.java
@@ -0,0 +1,62 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+package tarea1;
+public class tarea1 {
+		
+    public static void main(String[] args) {
+        int numeroEntero, sumaInt = 0;
+        double numeroDouble, sumaDouble = 0;
+
+        File f = new File("scp1.txt");
+
+        try  (Scanner entrada = new Scanner(f)) {
+        	int nRestri = entrada.nextInt();
+          System.out.println(nRestri); //retricciones 5
+        	int nMatriz = entrada.nextInt();
+          System.out.println(nMatriz);  //variables
+
+          //se hará una matriz de 5 filas y 6 restricc
+        	boolean restriciones[][]= new boolean[nRestri][nMatriz]; //matriz booleana
+
+        	int costo[]= new int [nMatriz]; //almacena los costos 65 55 60 45 40 50                   
+
+            for(int i=0;i<nMatriz;i++) { //relleno los costos
+  
+               	costo[i] = entrada.nextInt(); 
+                //System.out.println(costo[i]);
+  			    }
+
+
+  			for(int i=0;i<nRestri;i++) {  //ultima para tomar las restricciones
+               int arradidad = entrada.nextInt(); // sirve para detenernos en cada restriccion
+               System.out.println("aridad: "+arradidad);
+              for(int j=0;j<arradidad;j++) {
+                int wea = entrada.nextInt(); //guarda las variables activas en las restricciones
+                System.out.println("wea: "+wea);
+                restriciones[i][wea-1]=true; // nuestras restriciones
+  				    }
+  			}
+
+        mostrarRestricciones(restriciones,nRestri,nMatriz);
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void mostrarRestricciones(boolean restriciones[][],int nRestri,int nMatriz){
+      for (int i=0;i<nRestri;i++) {
+        for (int j=0;i<nMatriz;j++) {
+          System.out.print(restriciones[i][j]);
+          System.out.print(" ");
+        }
+        System.out.println();
+      }
+    }
+     	
+}
