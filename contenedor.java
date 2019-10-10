4dbf53b67289f561c37d027ec8b92e9884995d2b
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2019-10-10 13:50:38 -0300

    asd

--- contenedor.java
+++ contenedor.java
@@ -3,9 +3,10 @@ public class contenedor(){
      		public int valor;
      		public int cantidad[];
  			public boolean variables[];
- 			
+
  			public contenedor(boolean variables[],int costo[]){
- 					
+ 					// implementar un metodo que segun las variables entrantes y el costo calcule la combinacion mas barata
+ 			
 
  				}
      }
